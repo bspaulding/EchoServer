3f687ef24bd8961064282e00ae0a29baf8ad0d4c
Bradley Spaulding <bspaulding@dataxu.com>
2011-10-25 18:24:44 -0400

    Writing request to logs.

--- echo_server.rb
+++ echo_server.rb
@@ -2,7 +2,37 @@ require 'rubygems'
 require 'rack'
 
 class EchoServer
+  def initialize(options = {})
+    Dir.mkdir('log') unless File.exists?('log')
+  end
+
   def call(env)
+    log(env)
     [200, {"Content-Type" => "text/plain"}, env.inspect]
+  end
+
+private
+  
+  def log(hash, level = 0, file = nil)
+    root_call = file.nil?
+    if root_call
+      file = File.open('log/server.log', 'a')
+      file.write("[#{Time.now}]\n")
+    end
+    
+    hash.keys.each do |key|
+      if hash[key].is_a?(Hash)
+        log(hash[key], (level + 1), file)
+      else
+        if key == "rack.input"
+          value = hash[key].read
+        else
+          value = hash[key]
+        end
+        file.write("  #{' ' * level}#{key} => #{value}\n")
+      end
+    end
+
+    file.close if root_call
   end
 end
